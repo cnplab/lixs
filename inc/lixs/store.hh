7fc4d3b8e22104d4bdc1df0cfd869ccbec5e31d5
Filipe Manco <filipe.manco@neclab.eu>
2015-01-19 17:15:51 +0100

    store: rename id to tid for clarity

--- inc/lixs/store.hh
+++ inc/lixs/store.hh
@@ -9,16 +9,16 @@ namespace lixs {
 
 class store {
 public:
-    virtual void branch(unsigned int& id) = 0;
-    virtual void merge(unsigned int id, bool& success) = 0;
-    virtual void abort(unsigned int id) = 0;
+    virtual void branch(unsigned int& tid) = 0;
+    virtual void merge(unsigned int tid, bool& success) = 0;
+    virtual void abort(unsigned int tid) = 0;
 
-    virtual int create(unsigned int id, std::string key, bool& created) = 0;
-    virtual int read(unsigned int id, std::string key, std::string& val) = 0;
-    virtual int update(unsigned int id, std::string key, std::string val) = 0;
-    virtual int del(unsigned int id, std::string key) = 0;
+    virtual int create(unsigned int tid, std::string key, bool& created) = 0;
+    virtual int read(unsigned int tid, std::string key, std::string& val) = 0;
+    virtual int update(unsigned int tid, std::string key, std::string val) = 0;
+    virtual int del(unsigned int tid, std::string key) = 0;
 
-    virtual int get_children(unsigned int id, std::string key, std::list<std::string>& resp) = 0;
+    virtual int get_children(unsigned int tid, std::string key, std::list<std::string>& resp) = 0;
 };
 
 } /* namespace lixs */
