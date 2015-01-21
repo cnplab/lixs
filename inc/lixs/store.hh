7df17abaae4ea92b8e55210bde63c92b5b281c97
Filipe Manco <filipe.manco@neclab.eu>
2015-01-21 09:42:19 +0100

    store: rename key to path for clarity

--- inc/lixs/store.hh
+++ inc/lixs/store.hh
@@ -13,12 +13,12 @@ public:
     virtual void merge(unsigned int tid, bool& success) = 0;
     virtual void abort(unsigned int tid) = 0;
 
-    virtual int create(unsigned int tid, std::string key, bool& created) = 0;
-    virtual int read(unsigned int tid, std::string key, std::string& val) = 0;
-    virtual int update(unsigned int tid, std::string key, std::string val) = 0;
-    virtual int del(unsigned int tid, std::string key) = 0;
+    virtual int create(unsigned int tid, std::string path, bool& created) = 0;
+    virtual int read(unsigned int tid, std::string path, std::string& val) = 0;
+    virtual int update(unsigned int tid, std::string path, std::string val) = 0;
+    virtual int del(unsigned int tid, std::string path) = 0;
 
-    virtual int get_children(unsigned int tid, std::string key, std::set<std::string>& resp) = 0;
+    virtual int get_children(unsigned int tid, std::string path, std::set<std::string>& resp) = 0;
 };
 
 } /* namespace lixs */
