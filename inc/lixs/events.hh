dcb3cc32ba6148b1ffb30b6e4910b556efd2190e
Filipe Manco <filipe.manco@neclab.eu>
2015-02-03 14:48:07 +0100

    events: code review/cleanup

--- inc/lixs/events.hh
+++ inc/lixs/events.hh
@@ -9,7 +9,7 @@ namespace lixs {
 struct fd_cb_k {
     fd_cb_k(void)
         : fd(-1), ev_read(false), ev_write(false)
-    { };
+    { }
 
     /* FIXME: Provide information about errors (e.g. EPOLLERR) */
     virtual void operator() (bool ev_read, bool ev_write) = 0;
@@ -22,7 +22,7 @@ struct fd_cb_k {
 struct watch_cb_k {
     watch_cb_k(const std::string& path, const std::string& token, bool relative)
         : path(path), token(token), relative(relative)
-    { };
+    { }
 
     virtual void operator()(const std::string& path) = 0;
 
