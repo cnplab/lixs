ad7d4e714bd8fc8921b669a16dd168d67a1af46e
Filipe Manco <filipe.manco@neclab.eu>
2015-02-06 11:35:12 +0100

    event_mgr: remove watch interface

--- inc/lixs/event_mgr.hh
+++ inc/lixs/event_mgr.hh
@@ -1,7 +1,6 @@
 #ifndef __LIXS_EVENT_MGR_HH__
 #define __LIXS_EVENT_MGR_HH__
 
-#include <lixs/events.hh>
 #include <lixs/iomux.hh>
 
 #include <list>
@@ -18,26 +17,16 @@ public:
     event_mgr(iomux& io);
     ~event_mgr();
 
-    /* FIXME: Remove watches
-     *
-     * The event_mgr should only be aware of callbacks to be called ASAP
-     */
-
     void run(void);
 
     void enqueue_event(std::function<void(void)> cb);
 
-    void enqueue_watch(watch_cb_k& cb, const std::string& path);
-    void dequeue_watch(watch_cb_k& cb);
-
 private:
     void fire_events(void);
-    void fire_watches(void);
 
 private:
     iomux& io;
     std::list<std::function<void(void)> > event_list;
-    std::map<watch_cb_k*, std::set<std::string> > watch_list;
 };
 
 } /* namespace lixs */
--- lib/lixs/event_mgr.cc
+++ lib/lixs/event_mgr.cc
@@ -1,5 +1,7 @@
 #include <lixs/event_mgr.hh>
-#include <lixs/events.hh>
+#include <lixs/iomux.hh>
+
+#include <memory>
 
 
 lixs::event_mgr::event_mgr(iomux& io)
@@ -14,23 +16,12 @@ lixs::event_mgr::~event_mgr()
 void lixs::event_mgr::run(void)
 {
     fire_events();
-    fire_watches();
     io.handle();
 }
 
 void lixs::event_mgr::enqueue_event(std::function<void(void)> cb)
 {
     event_list.push_front(cb);
-}
-
-void lixs::event_mgr::enqueue_watch(watch_cb_k& cb, const std::string& path)
-{
-    watch_list[&cb].insert(path);
-}
-
-void lixs::event_mgr::dequeue_watch(watch_cb_k& cb)
-{
-    watch_list.erase(&cb);
 }
 
 void lixs::event_mgr::fire_events(void)
@@ -42,19 +33,5 @@ void lixs::event_mgr::fire_events(void)
     }
 
     event_list.clear();
-}
-
-void lixs::event_mgr::fire_watches(void)
-{
-    std::map<watch_cb_k*, std::set<std::string> >::iterator i;
-    std::set<std::string>::iterator j;
-
-    for (i = watch_list.begin(); i != watch_list.end(); i++) {
-        for (j = i->second.begin(); j != i->second.end(); j++) {
-            i->first->operator()(*j);
-        }
-    }
-
-    watch_list.clear();
 }
 
