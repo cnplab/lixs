dc1dab972727782669c3cd7114126887c62d8fec
Filipe Manco <filipe.manco@neclab.eu>
2015-11-12 21:57:00 +0100

    xs_proto_v1: move protocol impl out of client
    A new namespace was created for the implementation of the upstream
    version of the xenstore protocol (here called v1). This makes the
    implementation of new (future) protocols easier by separating what is
    specific to the protocol from the rest.
    
    Arguably the client class isn't necessary anymore, given pretty much all
    of its content was moved to protocol. However it will be kept for now,
    given it can be used in the future to implement protocol independent
    functionality.
    
    The new implementation is very similar to the previous one in the client
    class. However it brings the following improvements:
    
    * Cleaner code
    * Better error checking/handling
    * Independent handling of tx and rx

---
+++ lib/lixs/xs_proto_v1/watch.cc
@@ -0,0 +1,24 @@
+#include <lixs/xs_proto_v1/xs_proto.hh>
+
+
+lixs::xs_proto_v1::watch_cb::watch_cb(xs_proto_base& proto,
+        const std::string& path, const std::string& token, bool relative)
+    : lixs::watch_cb_k(path, token, relative), proto(proto)
+{
+}
+
+void lixs::xs_proto_v1::watch_cb::operator()(const std::string& fire_path)
+{
+    std::string path;
+
+    if (relative) {
+        /* NOTE: Remove dom_path plus '/' */
+        path = fire_path.substr(proto.dom_path.length() + 1);
+    } else {
+        path = fire_path;
+    }
+
+    proto.tx_queue.push_back({XS_WATCH_EVENT, 0, 0, { path, token }, true});
+    proto.process_tx();
+}
+
