9e0cc88da4ab12398f18bd09cf96778a4a621343
Filipe Manco <filipe.manco@neclab.eu>
2015-11-25 21:30:08 +0100

    mstore: fix transaction logic
    This commit fixes the transaction handling logic, hopefully fixing a lot
    of situations where transactions were not working properly.
    
    There are two major changes:
    
    * Uniformed handling of transaction entries
    * Simplified the algorithm deciding whether the transaction can commit
    
    This hopefully fixes #13.

--- lib/lixs/mstore/simple_access.cc
+++ lib/lixs/mstore/simple_access.cc
@@ -14,17 +14,19 @@ lixs::mstore::simple_access::simple_access(database& db)
 
 int lixs::mstore::simple_access::create(cid_t cid, const std::string& path, bool& created)
 {
-    ensure_branch(cid, path);
-
     record& rec = db[path];
 
     if (rec.e.write_seq) {
+        rec.e.read_seq = rec.next_seq++;
         created = false;
     } else {
+        ensure_branch(cid, path);
+        register_with_parent(path);
+
         rec.e.write_seq = rec.next_seq++;
+        rec.e.delete_seq = 0;
         get_parent_perms(path, rec.e.perms);
 
-        register_with_parent(path);
         created = true;
     }
 
@@ -36,12 +38,20 @@ int lixs::mstore::simple_access::read(cid_t cid, const std::string& path, std::s
     database::iterator it;
 
     it = db.find(path);
-    if (it != db.end() && it->second.e.write_seq) {
-        if (!has_read_access(cid, it->second.e.perms)) {
+    if (it == db.end()) {
+        return ENOENT;
+    }
+
+    record& rec = it->second;
+
+    rec.e.read_seq = rec.next_seq++;
+
+    if (rec.e.write_seq) {
+        if (!has_read_access(cid, rec.e.perms)) {
             return EACCES;
         }
 
-        val = it->second.e.value;
+        val = rec.e.value;
         return 0;
     } else {
         return ENOENT;
@@ -52,13 +62,13 @@ int lixs::mstore::simple_access::update(cid_t cid, const std::string& path, cons
 {
     record& rec = db[path];
 
-    if (!has_write_access(cid, rec.e.perms)) {
-        return EACCES;
-    }
-
-    ensure_branch(cid, path);
-
-    if (!rec.e.write_seq) {
+    if (rec.e.write_seq) {
+        if (!has_write_access(cid, rec.e.perms)) {
+            rec.e.read_seq = rec.next_seq++;
+            return EACCES;
+        }
+    } else {
+        ensure_branch(cid, path);
         register_with_parent(path);
 
         get_parent_perms(path, rec.e.perms);
@@ -76,23 +86,30 @@ int lixs::mstore::simple_access::del(cid_t cid, const std::string& path)
     database::iterator it;
 
     it = db.find(path);
-    if (it != db.end() && it->second.e.write_seq) {
-        if (!has_write_access(cid, it->second.e.perms)) {
+    if (it == db.end()) {
+        return ENOENT;
+    }
+
+    record& rec = it->second;
+
+    if (rec.e.write_seq) {
+        if (!has_write_access(cid, rec.e.perms)) {
+            rec.e.read_seq = rec.next_seq++;
             return EACCES;
         }
 
         delete_branch(path);
-
         unregister_from_parent(path);
 
-        if (it->second.te.size() == 0) {
-            db.erase(path);
+        if (rec.te.empty()) {
+            db.erase(it);
         } else {
-            it->second.e.write_seq = 0;
-            it->second.e.delete_seq = it->second.next_seq++;
+            rec.e.write_seq = 0;
+            rec.e.delete_seq = rec.next_seq++;
         }
         return 0;
     } else {
+        rec.e.read_seq = rec.next_seq++;
         return ENOENT;
     }
 }
@@ -101,17 +118,22 @@ int lixs::mstore::simple_access::get_children(cid_t cid,
         const std::string& path, std::set<std::string>& resp)
 {
     database::iterator it;
-    std::set<std::string>::iterator i;
 
     it = db.find(path);
-    if (it != db.end() && it->second.e.write_seq) {
-        if (!has_read_access(cid, it->second.e.perms)) {
+    if (it == db.end()) {
+        return ENOENT;
+    }
+
+    record& rec = it->second;
+
+    rec.e.read_seq = rec.next_seq++;
+
+    if (rec.e.write_seq) {
+        if (!has_read_access(cid, rec.e.perms)) {
             return EACCES;
         }
 
-        for (i = it->second.e.children.begin(); i != it->second.e.children.end(); i++) {
-            resp.insert(*i);
-        }
+        resp.insert(rec.e.children.begin(), rec.e.children.end());
 
         return 0;
     } else {
@@ -125,13 +147,20 @@ int lixs::mstore::simple_access::get_perms(cid_t cid,
     database::iterator it;
 
     it = db.find(path);
-    if (it != db.end() && it->second.e.write_seq) {
-        if (!has_read_access(cid, it->second.e.perms)) {
+    if (it == db.end()) {
+        return ENOENT;
+    }
+
+    record& rec = it->second;
+
+    rec.e.read_seq = rec.next_seq++;
+
+    if (rec.e.write_seq) {
+        if (!has_read_access(cid, rec.e.perms)) {
             return EACCES;
         }
 
-        perms = it->second.e.perms;
-
+        perms = rec.e.perms;
         return 0;
     } else {
         return ENOENT;
@@ -144,15 +173,24 @@ int lixs::mstore::simple_access::set_perms(cid_t cid,
     database::iterator it;
 
     it = db.find(path);
-    if (it != db.end() && it->second.e.write_seq) {
-        if (!has_write_access(cid, it->second.e.perms)) {
+    if (it == db.end()) {
+        return ENOENT;
+    }
+
+    record& rec = it->second;
+
+    if (rec.e.write_seq) {
+        if (!has_write_access(cid, rec.e.perms)) {
+            rec.e.read_seq = rec.next_seq++;
             return EACCES;
         }
 
         it->second.e.perms = perms;
+        rec.e.write_seq = rec.next_seq++;
 
         return 0;
     } else {
+        rec.e.read_seq = rec.next_seq++;
         return ENOENT;
     }
 }
@@ -179,17 +217,12 @@ void lixs::mstore::simple_access::unregister_from_parent(const std::string& path
 
 void lixs::mstore::simple_access::ensure_branch(cid_t cid, const std::string& path)
 {
-    size_t pos;
     bool created;
+    std::string name;
+    std::string parent;
 
-    pos = path.rfind('/');
-    if (pos != std::string::npos) {
-        std::string parent = path.substr(0, pos);
-        record& rec = db[parent];
-
-        if (!rec.e.write_seq) {
-            create(cid, parent, created);
-        }
+    if (basename(path, parent, name)) {
+        create(cid, parent, created);
     }
 }
 
@@ -211,13 +244,11 @@ void lixs::mstore::simple_access::delete_branch(const std::string& path)
 
 void lixs::mstore::simple_access::get_parent_perms(const std::string& path, permission_list& perms)
 {
-    size_t pos;
+    std::string name;
+    std::string parent;
     database::iterator it;
 
-    pos = path.rfind('/');
-    if (pos != std::string::npos) {
-        std::string parent = path.substr(0, pos);
-
+    if (basename(path, parent, name)) {
         it = db.find(parent);
         if (it != db.end() && it->second.e.write_seq) {
             perms = it->second.e.perms;
