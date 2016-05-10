822bb272a37c7a5da2b47f390882357ddc11b821
Filipe Manco <filipe.manco@neclab.eu>
2016-05-10 17:36:02 +0200

    Makefile: simplify verbose commands
    Re-use the definition of cmd to do verbose/non-verbose builds.

--- make.mk
+++ make.mk
@@ -1,23 +1,18 @@
-CCOMPILE	 = $(CC) $(CFLAGS) -c $(1) -o $(2)
-CLINK		 = $(CC) $(CFLAGS) $(1) $(LDFLAGS) -o $(2)
-CXXCOMPILE	 = $(CXX) $(CXXFLAGS) -c $(1) -o $(2)
-CXXLINK		 = $(CXX) $(CXXFLAGS) $(1) $(LDFLAGS) -o $(2)
-
+# Define verbose command
 ifneq ($(verbose),y)
-ccompile	 = @printf " %-4s %s\n" "CC"  $@ && $(CCOMPILE)
-clink		 = @printf " %-4s %s\n" "LD"  $@ && $(CLINK)
-cxxcompile	 = @printf " %-4s %s\n" "CXX" $@ && $(CXXCOMPILE)
-cxxlink		 = @printf " %-4s %s\n" "LD"  $@ && $(CXXLINK)
 cmd			 = @printf " %-4s %s\n" $(1) $(2) && $(3) $(4)
 else
-ccompile	 = $(CCOMPILE)
-link		 = $(CLINK)
-cxxcompile	 = $(CXXCOMPILE)
-cxxlink		 = $(CXXLINK)
 cmd			 = $(3) $(4)
 endif
 
+# Compile and link commands
+ccompile	 = $(call cmd, "CC", $2, $(CC), $(CFLAGS) -c $(1) -o $(2))
+clink		 = $(call cmd, "LD", $2, $(CC), $(CFLAGS) $(1) $(LDFLAGS) -o $(2))
+cxxcompile	 = $(call cmd, "CXX", $2, $(CXX), $(CXXFLAGS) -c $(1) -o $(2))
+cxxlink		 = $(call cmd, "LD", $2, $(CXX), $(CXXFLAGS) $(1) $(LDFLAGS) -o $(2))
 
+
+# Default build rules for objects
 %.o: %.cc
 	$(call cxxcompile, $<, $@)
 
