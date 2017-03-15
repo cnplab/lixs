c8138d2582a18ae3a5f265c567456b9bd4e8b515
Filipe Manco <filipe.manco@neclab.eu>
2017-03-15 16:04:16 +0100

    client: use shared_ptr for dependencies

--- inc/lixs/client.hh
+++ inc/lixs/client.hh
@@ -41,6 +41,7 @@
 
 #include <cstdio>
 #include <string>
+#include <memory>
 #include <utility>
 
 
@@ -50,7 +51,7 @@ template < typename PROTOCOL >
 class client : public PROTOCOL {
 public:
     template < typename... ARGS >
-    client(const std::string& cid, log::logger& log, ARGS&&... args);
+    client(const std::string& cid, const std::shared_ptr<log::logger>& log, ARGS&&... args);
     virtual ~client();
 
 private:
@@ -58,22 +59,23 @@ private:
 
 private:
     std::string client_id;
-    log::logger& log;
+    std::shared_ptr<log::logger> log;
 };
 
 
 template < typename PROTOCOL >
 template < typename... ARGS >
-client<PROTOCOL>::client(const std::string& cid, log::logger& log, ARGS&&... args)
+client<PROTOCOL>::client(const std::string& cid, const std::shared_ptr<log::logger>& log,
+        ARGS&&... args)
     : PROTOCOL(std::forward<ARGS>(args)...), client_id(cid), log(log)
 {
-    log::LOG<log::level::INFO>::logf(log, "[%4s] New client registered", client_id.c_str());
+    log::LOG<log::level::INFO>::logf(*log, "[%4s] New client registered", client_id.c_str());
 }
 
 template < typename PROTOCOL >
 client<PROTOCOL>::~client()
 {
-    log::LOG<log::level::INFO>::logf(log, "[%4s] Client connection closed", client_id.c_str());
+    log::LOG<log::level::INFO>::logf(*log, "[%4s] Client connection closed", client_id.c_str());
 }
 
 template < typename PROTOCOL >
